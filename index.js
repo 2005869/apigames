0bb7ffaf8d97e07827d288a629afdf30011bdd73
2005869 <2005869@aluno.univesp.br>
2022-12-10 10:24:18 -0300

    remove JSON DB

--- index.js
+++ index.js
@@ -7,34 +7,6 @@ const bodyParser = require('body-parser');
 app.use(bodyParser.urlencoded({extended: false}));
 app.use(bodyParser.json());
 
-var DB = {
-    games: [
-        {
-            id: 23,
-            name: 'Pitfall',
-            year: 1980,
-            price: 80
-        },
-        {
-            id: 54,
-            name: 'Enduro',
-            year: 1982,
-            price: 100
-        },
-        {
-            id: 13,
-            name: 'PacMan',
-            year: 1978,
-            price: 90
-        },
-        {
-            id: 83,
-            name: 'Tetris',
-            year: 1985,
-            price: 150
-        },
-    ]
-}
 
 //Rotes
 
