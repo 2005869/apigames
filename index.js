09d1d2b546d7b22cd73c1fc346810bb8f7df75dc
2005869 <2005869@aluno.univesp.br>
2022-12-10 11:20:35 -0300

    add axios in project, and front end to list and add games

--- index.js
+++ index.js
@@ -1,11 +1,13 @@
 const express = require('express');
 const app = express();
 const game = require('./database/database');
+const cors = require('cors');
 
 const bodyParser = require('body-parser');
 
 app.use(bodyParser.urlencoded({extended: false}));
 app.use(bodyParser.json());
+app.use(cors());
 
 
 //Rotes
