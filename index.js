1eec22882edcf5da6ce4837b4f30b331388dc765
2005869 <2005869@aluno.univesp.br>
2022-12-16 16:21:05 -0300

    remove a problem

--- index.js
+++ index.js
@@ -2,7 +2,6 @@ const express = require('express');
 const app = express();
 const game = require('./database/database');
 const cors = require('cors');
-const UsersController = require('./users/UsersController');
 
 const bodyParser = require('body-parser');
 
@@ -10,7 +9,6 @@ app.use(bodyParser.urlencoded({extended: false}));
 app.use(bodyParser.json());
 app.use(cors());
 
-app.use('/', UsersController);
 
 //Rotes
 
